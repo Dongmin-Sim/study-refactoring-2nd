5762d45f720feec7a0a9a1e1f1ac22674e25c94b
codongmin <codongmin@gmail.com>
2025-01-23 08:59:24 +0900

    Refactor: htmlStatement() 추가

--- src/main/java/chapter1/Statement.java
+++ src/main/java/chapter1/Statement.java
@@ -19,6 +19,10 @@ public class Statement {
         return renderPlainText(StatementData.createStatementData(invoice, plays));
     }
 
+    public String htmlStatement() {
+        return renderHtml(StatementData.createStatementData(invoice, plays));
+    }
+
     private String renderPlainText(StatementData statementData) {
         String result = String.format("청구 내역 (고객명: %s)\n", statementData.getCustomer());
 
@@ -30,6 +34,21 @@ public class Statement {
         result += String.format("총액: %s\n", usd(statementData.getTotalAmount()));
         result += String.format("적립 포인트: %d점", statementData.getTotalVolumeCredits());
 
+        return result;
+    }
+
+    private String renderHtml(StatementData statementData) {
+        String result = String.format("<h1>청구 내역 (고객명: %s)</h1>\n", statementData.getCustomer());
+        result += "<table>\n";
+        result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+        for (EnrichPerformance perf : statementData.getPerformances()) {
+            // 청구 내역을 출력한다.
+            result += String.format("  <tr><td>%s</td><td>(%d석)</td>", perf.play().name(), perf.audience());
+            result += String.format("<td>%s</td></tr>\n", usd(perf.amount()));
+        }
+        result += "</table>\n";
+        result += String.format("<p>총액: <em>%s</em></p>\n", usd(statementData.getTotalAmount()));
+        result += String.format("<p>적립 포인트: <em>%d</em>점</p>", statementData.getTotalVolumeCredits());
         return result;
     }
 
