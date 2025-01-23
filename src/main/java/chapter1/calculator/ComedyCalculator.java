b75bf98866c50aee62eb4795b7951552150ac517
codongmin <codongmin@gmail.com>
2025-01-23 09:59:31 +0900

    Refactor: PerformanceCalculator 서브 클래스 추가

---
+++ src/main/java/chapter1/calculator/ComedyCalculator.java
@@ -0,0 +1,12 @@
+package chapter1.calculator;
+
+import chapter1.PerformanceCalculator;
+import chapter1.data.Performance;
+import chapter1.data.Play;
+
+public class ComedyCalculator extends PerformanceCalculator {
+
+    public ComedyCalculator(Performance performance, Play play) {
+        super(performance, play);
+    }
+}
---
+++ src/main/java/chapter1/calculator/TragedyCalculator.java
@@ -0,0 +1,12 @@
+package chapter1.calculator;
+
+import chapter1.PerformanceCalculator;
+import chapter1.data.Performance;
+import chapter1.data.Play;
+
+public class TragedyCalculator extends PerformanceCalculator {
+
+    public TragedyCalculator(Performance performance, Play play) {
+        super(performance, play);
+    }
+}
